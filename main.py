afcd884e3d6d9e848ac6625538bfc2a2b02c0ed4
enriquegzmn <enriquegzmnvelarde@gmail.com>
2025-08-25 00:06:14 -0400

    kkk

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
-    return file_contents
+        return file_contents
 
 
 def main():
-    get_book_text("/home/enriqueguzman/Documents/Workspace/bootdev/bookbot/books/frankenstein.txt")
+   text = get_book_text("books/frankenstein.txt")
+   print(text)
 
 
