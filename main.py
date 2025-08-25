50bbff34fba971ebd1d5cf277b8ad6d34fee6c87
enriquegzmn <enriquegzmnvelarde@gmail.com>
2025-08-24 23:57:51 -0400

    EXERCISE 2

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    get_book_text("/home/enriqueguzman/Documents/Workspace/bootdev/bookbot/books/frankenstein.txt")
+
+
