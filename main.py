8b70557591674d52db59a294d7bd58d3d36785f1
enriquegzmn <enriquegzmnvelarde@gmail.com>
2025-08-25 00:26:25 -0400

    Exercise 3

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+def main():
+   text = get_book_text("books/frankenstein.txt")
+   print(number_or_words(text))
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
+def number_or_words(text):
+    words_list = text.split()
+    counter = 0
+    for i in words_list:
+        counter += 1
+    
+    return f"{counter} words found in the document"
+    
 
-def main():
-   text = get_book_text("books/frankenstein.txt")
-   print(text)
+
+
 
 main()
 
